084d42928b8e1b99589fd0946ee7515dd8aa57b0
Emiliana Goldsack <emigoldsack@gmail.com>
2023-09-07 19:00:04 -0300

    Segundo Entregable

--- entregable1.py
+++ entregable1.py
@@ -1,79 +1,92 @@
-from datetime import datetime
+import pandas as pd
 import json
 import requests
 import psycopg2
+from datetime import datetime
 
 # URL de la API
 url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales'
+
+# Consulta SQL como constante
+CREATE_TABLE_QUERY = """
+    CREATE TABLE IF NOT EXISTS cotizaciones (
+        nombre VARCHAR(255),
+        compra NUMERIC,
+        venta NUMERIC,
+        fecha DATE,
+        PRIMARY KEY (nombre, fecha)
+    )
+"""
 
 try:
     # Realizar la solicitud HTTP a la API
     response = requests.get(url)
 
-    # Verificar si la solicitud fue exitosa (código de respuesta 200)
+    # Verificar si la solicitud fue exitosa 
     if response.status_code == 200:
         # Obtener los datos JSON
         cotizaciones = response.json()
 
         # Filtrar las cotizaciones para excluir ciertas monedas
-        cotizaciones_filtradas = [casa for casa in cotizaciones if casa['casa']['nombre'] not in ['Argentina', 'Bitcoin', 'Dolar Soja']]
+        cotizaciones_filtradas = [casa['casa'] for casa in cotizaciones if casa['casa']['nombre'] not in ['Argentina', 'Bitcoin', 'Dolar Soja']]
+
+        # Crear un DataFrame con los datos
+        df = pd.DataFrame(cotizaciones_filtradas)
 
         # Cargar credenciales desde el archivo de configuración
         with open("config.json", "r") as config_file:
-          config = json.load(config_file)
+            config = json.load(config_file)
 
-        db_host = config["db_host"]
-        db_port = config["db_port"]
-        db_name = config["db_name"]
-        db_user = config["db_user"]
-        db_password = config["db_password"]
-
-        # Conectar a la base de datos usando las credenciales obtenidas
+        # Crear una conexión a Redshift utilizando psycopg2
         conn = psycopg2.connect(
-            host=db_host,
-            port=db_port,
-            database=db_name,
-            user=db_user,
-            password=db_password
+            dbname=config['db_name'],
+            user=config['db_user'],
+            password=config['db_password'],
+            host=config['db_host'],
+            port=config['db_port']
         )
-        # Crear un cursor
+
+        # Crear un cursor para ejecutar consultas
         cursor = conn.cursor()
 
-        for cotizacion in cotizaciones_filtradas:
-            nombre = cotizacion['casa']['nombre']
-            compra = cotizacion['casa']['compra']
-            venta = cotizacion['casa']['venta']
+        # Ejecutar la consulta SQL para crear la tabla
+        cursor.execute(CREATE_TABLE_QUERY)
 
-            # Modificar los valores numéricos con comas por puntos
-            if compra != 'No Cotiza':
-                compra = compra.replace(',', '.')
-            else:
-                compra = '0'  # Valor predeterminado para 'No Cotiza'
-
-            if venta != 'No Cotiza':
-                venta = venta.replace(',', '.')
-            else:
-                venta = '0'  # Valor predeterminado para 'No Cotiza'
-
-            # Obtener la fecha y hora actual
-            fecha = datetime.now()
-
-            # Comprobar si el registro ya existe en la tabla
-            cursor.execute("SELECT COUNT(*) FROM cotizaciones WHERE nombre = %s", (nombre,))
-            existe_registro = cursor.fetchone()[0]
-
-            if existe_registro > 0:
-                # Si el registro existe, actualizarlo en lugar de insertarlo
-                cursor.execute("UPDATE cotizaciones SET compra = %s, venta = %s, fecha = %s WHERE nombre = %s", (compra, venta, fecha, nombre))
-            else:
-                # Si el registro no existe, insertarlo
-                cursor.execute("INSERT INTO cotizaciones (nombre, compra, venta, fecha) VALUES (%s, %s, %s, %s)", (nombre, compra, venta, fecha))
-
-        # Commit y cerrar la conexión
+        # Confirmar la creación de la tabla
         conn.commit()
+
+        # Obtener la fecha actual
+        fecha_actual = datetime.now().date()
+
+        
+        # Reemplazar comas por puntos en las columnas 'compra' y 'venta'
+        df['compra'] = df['compra'].str.replace(',', '.')
+        df['venta'] = df['venta'].str.replace(',', '.')
+
+        # Reemplazar 'No Cotiza' por 0 en las columnas 'compra' y 'venta'
+        df['compra'] = df['compra'].replace('No Cotiza', '0')
+        df['venta'] = df['venta'].replace('No Cotiza', '0')
+
+        # Convertir las columnas a tipo float
+        df['compra'] = df['compra'].astype(float)
+        df['venta'] = df['venta'].astype(float)
+
+        # Cargar los datos en Redshift con la fecha actual
+        for index, row in df.iterrows():
+         cursor.execute(
+          "INSERT INTO cotizaciones (nombre, compra, venta, fecha) VALUES (%s, %s, %s, %s)",
+        (row['nombre'], row['compra'], row['venta'], fecha_actual)
+        )
+
+        # Confirmar la inserción de los datos
+        conn.commit()
+
+        # Cerrar el cursor y la conexión
+        cursor.close()
         conn.close()
+
+        print("Datos cargados exitosamente en la base de datos.")
     else:
         print(f'Error en la solicitud: {response.status_code}')
 except Exception as e:
-    print(f'Error al conectarse a la API o a Redshift: {str(e)}')
-
+    print(f'Error al conectarse a la API o a la base de datos: {str(e)}')
